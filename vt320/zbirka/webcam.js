65d59340573a31885923e86f1dfb8f6477177be5
Marko Stamcar <marko@stamcar.com>
2020-06-10 18:41:53 +0200

    Izboljsave za tisk webcam ascii art

--- vt320/zbirka/webcam.js
+++ vt320/zbirka/webcam.js
@@ -4,19 +4,23 @@ var Camera = require("@sigodenjs/fswebcam");
 var camera = new Camera({
     callbackReturn: "buffer"
 });
+var gm = require('gm');
 
 const requestListener = function(req, res) {
     res.writeHead(200);
     camera.capture(function(err, data) {
-        imageToAscii(data, {
-            colored: false,
-            size_options: {
-                screen_size: {
-                    height: 30
+        gm(data).edge(2).toBuffer('JPG', function(err, data) {
+            imageToAscii(data, {
+                colored: false,
+                reverse: false,
+                size_options: {
+                    screen_size: {
+                        height: 30
+                    }
                 }
-            }
-        }, (err, converted) => {
-            res.end(converted);
+            }, (err, converted) => {
+                res.end(converted);
+            });
         });
     });
 };
