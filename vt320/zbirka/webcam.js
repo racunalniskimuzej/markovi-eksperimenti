0e76abcbc657189c19ba57376af3687dc22d5593
Marko Stamcar <marko@stamcar.com>
2020-06-10 19:59:18 +0200

    Namesto edge(2) uporabi contrast(-2)

--- vt320/zbirka/webcam.js
+++ vt320/zbirka/webcam.js
@@ -2,20 +2,22 @@ const http = require("http");
 const imageToAscii = require("image-to-ascii");
 var Camera = require("@sigodenjs/fswebcam");
 var camera = new Camera({
-    callbackReturn: "buffer"
+    callbackReturn: "buffer",
+    rotate: 90
 });
 var gm = require('gm');
 
 const requestListener = function(req, res) {
     res.writeHead(200);
     camera.capture(function(err, data) {
-        gm(data).edge(2).toBuffer('JPG', function(err, data) {
+        gm(data).contrast(-5).toBuffer('JPG', function(err, data) {
             imageToAscii(data, {
                 colored: false,
-                reverse: false,
+                reverse: true,
+                pixels: " .,:;i1tfLCG08",
                 size_options: {
                     screen_size: {
-                        height: 30
+                        height: 52
                     }
                 }
             }, (err, converted) => {
