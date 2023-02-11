a5ce3670a249863c70c7e5100b58ac0b7a518f8f
markostamcar <marko@stamcar.com>
2023-02-11 10:50:39 +0100

    Added gb-upload.js

---
+++ vt320/zbirka/gb-upload.js
@@ -0,0 +1,48 @@
+require('./utils');
+require('./gb');
+const request = require('request');
+
+while (true) {
+    var serialport_wait = require('serialport-wait');
+    var serialport = new serialport_wait();
+
+    try {
+        serialport.connect('/dev/gameboy', 115200);
+
+        if (serialport.isOpen()) {
+            serialport.wait('INIT', 0);
+            if (serialport.get_wait_result()) {
+                serialport.wait('Timed Out', 30);
+                if (serialport.get_wait_result()) {
+                    gbp = render_gbp(serialport.get_buffer_all());
+
+                    const formData = {
+                        fajl: {
+                            value: gbp,
+                            options: {
+                                filename: 'gb.jpg',
+                                contentType: 'image/jpeg'
+                            }
+                        }
+                    };
+
+                    request.post({
+                        url: 'https://---------------/index.php',
+                        formData: formData
+                    });
+                    console.log("juhej!");
+                } else {
+                    throw "napaka1";
+                }
+            } else {
+                throw "napaka2";
+            }
+        } else {
+            throw "napaka3";
+        }
+    } catch (e2) {
+        console.log(e2);
+    }
+
+    if (serialport.isOpen()) serialport.close();
+}
