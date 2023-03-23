0fba58f16f9288cb76027eef9b779da01e63dbbe
markostamcar <marko@stamcar.com>
2023-03-23 21:11:12 +0100

    Delete gb-camera/gb-upload-php/upload directory

--- gb-camera/gb-upload-php/upload/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$files = array_values(array_diff(scandir('.', SCANDIR_SORT_DESCENDING), array('..', '.', 'index.php')));
-for ($i = 0; $i < min(10, count($files)); $i++) {
- echo "<p><img src='{$files[$i]}'></p>\n";
-}
